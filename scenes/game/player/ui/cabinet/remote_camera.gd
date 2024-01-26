24faf748fbe25b79530789397d4fc4b8a62bb6d1
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-01-26 09:14:57 -0600

    Impl. Screen and resolution

--- scenes/game/player/ui/cabinet/remote_camera.gd
+++ scenes/game/player/ui/cabinet/remote_camera.gd
@@ -1,5 +1,7 @@
 extends Camera3D
 
+# TODO: Switch this class to use PlayerBind so Entities can be spectated.
+
 func _process(delta: float) -> void:
 	if MainPlayerBody.instance:
 		global_position = MainPlayerBody.instance.camera_pivot.global_position
--- scenes/game/player/ui/cabinet/screen.gd
+++ scenes/game/player/ui/cabinet/screen.gd
@@ -1,11 +1,37 @@
-extends Control
+class_name Screen extends Control
 
+static func get_global_scale(node: Node) -> Vector2:
+		if !node:
+			return Vector2.ONE
+		var outer := get_global_scale(node.get_parent())
+		if node is Control:
+			return node.scale * outer
+		if node is Node2D:
+			return node.scale * outer
+		return outer
 
-# Called when the node enters the scene tree for the first time.
+@export var resolution_container: Control
+
+var scale_factor := 1.0:
+	set(x):
+		scale_factor = x
+		is_dirty = true
+var pixel_scale := Vector2.ONE:
+	set(x):
+		pixel_scale = x
+		is_dirty = true
+
+var is_dirty: bool
+
 func _ready() -> void:
-	pass # Replace with function body.
+	is_dirty = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if is_dirty:
+		var total_scale := scale_factor * pixel_scale
+		resolution_container.scale = Vector2(1.0 / total_scale.x, 1.0 / total_scale.y)
+		resolution_container.size = size * total_scale
+		is_dirty = false
+
+func match_window_resolution() -> void:
+	pixel_scale = get_global_scale(get_parent())
