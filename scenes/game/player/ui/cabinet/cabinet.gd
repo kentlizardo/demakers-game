1718c6278a7ef8ad0e1947063c5c8bd8e6c971f2
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-01-27 20:54:15 -0600

    Delete old ConsoleView once a new one is created

--- scenes/game/player/ui/cabinet/cabinet.gd
+++ scenes/game/player/ui/cabinet/cabinet.gd
@@ -7,7 +7,14 @@ var console_view: ConsoleView
 func _ready() -> void:
 	create_view(load("res://packs/demo/consoles/zero_sprite.tscn"))
 
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("primary"):
+		create_view(load("res://packs/demo/consoles/zero_sprite.tscn"))
+
 func create_view(con_sprite_template: PackedScene) -> void:
-	console_view = ConsoleView.new(con_sprite_template)
+	var new_view := ConsoleView.new(con_sprite_template)
+	if console_view:
+		console_view.delete_once_view_created(new_view)
+	console_view = new_view
 	console_view.target_region = target_region
 	add_child(console_view)
--- scenes/game/player/ui/cabinet/console_view.gd
+++ scenes/game/player/ui/cabinet/console_view.gd
@@ -7,29 +7,34 @@ const DEFAULT_SETTINGS := {
 
 static var settings := DEFAULT_SETTINGS
 
-var console_sprite: ConsoleSprite
+signal view_created
+
 var target_region: Control
-var screen: Screen
+
+var _console_sprite: ConsoleSprite
+var _screen: Screen
 
 func _init(console_sprite_template: PackedScene) -> void:
 	set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
 	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	if console_sprite_template:
-		console_sprite = console_sprite_template.instantiate() as ConsoleSprite
-		add_child(console_sprite)
+		_console_sprite = console_sprite_template.instantiate() as ConsoleSprite
+		add_child(_console_sprite)
 
 func _ready() -> void:
-	screen = SCREEN_PACKED.instantiate() as Screen
-	if console_sprite:
-		console_sprite.min_view_region.add_child(screen)
+	_screen = SCREEN_PACKED.instantiate() as Screen
+	if _console_sprite:
+		_console_sprite.min_view_region.add_child(_screen)
 	else:
-		add_child(screen)
+		add_child(_screen)
 	
 	resized.connect(_on_resize)
 	_on_resize()
 	
 	var tw := create_tween()
 	tw.tween_property(self, "scale", Vector2.ONE, 1.0).from(Vector2.ONE * 0.2).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
+	await tw.finished
+	view_created.emit()
 
 func _process(delta: float) -> void:
 	pivot_offset = size / 2
@@ -38,17 +43,23 @@ func _on_resize() -> void:
 	resize_sprite_and_screen.call_deferred()
 
 func resize_sprite_and_screen() -> void:
-	if console_sprite:
+	if _console_sprite:
 		if settings["RESIZE_MIN_VIEW_TO_TARGET"]:
 			if !target_region:
-				console_sprite.resize_min_view_to_viewport()
+				_console_sprite.resize_min_view_to_viewport()
 			else:
-				console_sprite.resize_min_view_to_target(target_region)
+				_console_sprite.resize_min_view_to_target(target_region)
 		else:
 			var target_pos: Vector2
 			if target_region:
 				target_pos = target_region.get_global_rect().get_center()
 			else:
 				target_pos = get_viewport_rect().get_center()
-			console_sprite.global_position = target_pos
-	screen.match_window_resolution()
+			_console_sprite.global_position = target_pos
+	_screen.match_window_resolution()
+
+func delete_once_view_created(view: ConsoleView) -> void:
+	if view:
+		if !view.is_queued_for_deletion():
+			await view.view_created
+	queue_free()
